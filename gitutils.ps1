a60ab900ad579a8bdab7e3850640d431f555e879
Mike Glenn <mglenn@truefitsolutions.com>
2011-04-20 21:32:30 -0400

    initial import

---
+++ gitutils.ps1
@@ -0,0 +1,86 @@
+# Git functions
+# Mark Embling (http://www.markembling.info/)
+
+# Is the current directory a git repository/working copy?
+function isCurrentDirectoryGitRepository {
+    if ((Test-Path ".git") -eq $TRUE) {
+        return $TRUE
+    }
+    
+    # Test within parent dirs
+    $checkIn = (Get-Item .).parent
+    while ($checkIn -ne $NULL) {
+        $pathToTest = $checkIn.fullname + '/.git'
+        if ((Test-Path $pathToTest) -eq $TRUE) {
+            return $TRUE
+        } else {
+            $checkIn = $checkIn.parent
+        }
+    }
+    
+    return $FALSE
+}
+
+function TagDeployment {
+	$date = Get-Date -format yyyy-MM-dd.HH.mm.ss
+	git tag -m "Deployed $date" deploy-$date
+	rv date
+}
+
+function DeleteTag {
+	Param([parameter(Mandatory=$true)][string]$tagname)
+	git tag -d $tagname
+	git push origin :refs/tags/$tagname
+}
+
+# Get the current branch
+function GitBranchName {
+    $currentBranch = git symbolic-ref HEAD
+		$index = $currentBranch.LastIndexOf('/') + 1
+		if($index -gt 0) {
+			return $currentBranch.Substring($index, $currentBranch.Length - $index)
+		}
+		return "merging"
+}
+
+# Extracts status details about the repo
+function gitStatus {
+    $untracked = $FALSE
+    $added = 0
+    $modified = 0
+    $deleted = 0
+    $ahead = $FALSE
+    $aheadCount = 0
+    
+    $output = git status
+    
+    $branchbits = $output[0].Split(' ')
+    $branch = $branchbits[$branchbits.length - 1]
+    
+    $output | foreach {
+        if ($_ -match "^\#.*origin/.*' by (\d+) commit.*") {
+            $aheadCount = $matches[1]
+            $ahead = $TRUE
+        }
+        elseif ($_ -match "deleted:") {
+            $deleted += 1
+        }
+        elseif (($_ -match "modified:") -or ($_ -match "renamed:")) {
+            $modified += 1
+        }
+        elseif ($_ -match "new file:") {
+            $added += 1
+        }
+        elseif ($_ -match "Untracked files:") {
+            $untracked = $TRUE
+        }
+    }
+    
+    return @{"untracked" = $untracked;
+             "added" = $added;
+             "modified" = $modified;
+             "deleted" = $deleted;
+             "ahead" = $ahead;
+             "aheadCount" = $aheadCount;
+             "branch" = $branch}
+}
---
+++ profile.ps1
@@ -0,0 +1,37 @@
+# My preferred prompt for Powershell. 
+# Displays git branch and stats when inside a git repository.
+
+# See http://gist.github.com/180853 for gitutils.ps1.
+$dir = Split-Path $MyInvocation.MyCommand.Path
+. (Join-Path $dir gitutils.ps1)
+
+function prompt {
+	$path = ([string]$pwd)
+	$index = $path.LastIndexOf('\') + 1
+	$userLocation = $path.Substring($index, $path.Length - $index)
+	
+	$host.UI.RawUi.WindowTitle = "Git - $pwd"
+	Write-Host($userLocation) -nonewline -foregroundcolor Green 
+	
+	if (isCurrentDirectoryGitRepository) {
+		$branch = GitBranchName()
+
+		Write-Host('[') -nonewline -foregroundcolor Yellow
+		if($branch -eq "merging") {
+			Write-Host("merging") -nonewline -foregroundcolor Red
+		}
+		else {
+			Write-Host($branch) -nonewline -foregroundcolor Cyan
+		}
+		Write-Host(']') -nonewline -foregroundcolor Yellow
+	}
+    
+	Write-Host('>') -nonewline -foregroundcolor Green    
+	return " "
+}
+
+function Open-EbizSolution {
+	Invoke-Expression "& `".\Ebiz 2007 Modules.sln`""
+}
+Set-Alias dev Open-EbizSolution
+Set-Alias which get-command
