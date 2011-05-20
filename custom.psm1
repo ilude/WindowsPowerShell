83f3b955c86f8e7609b7c235522c3035173ef635
Mike Glenn <mglenn@truefitsolutions.com>
2011-05-19 23:00:54 -0400

    updated nano alias, added Get-Editor

--- custom.psm1
+++ custom.psm1
@@ -2,6 +2,19 @@
 Set-Alias Get-Environment "Get-ChildItem Env:"
 Set-Alias Get-Version $Host.Version
 
+function Get-Editor {
+	$path = Resolve-Path ("$env:ProgramFiles*" + "\notepad*\notepad*");
+	if(Test-Path $path) {
+		return $path.Path;
+	}
+	
+	$path = Join-Path $env:windir "\system32\notepad.exe"
+	if(Test-Path $path) {
+		return $path;
+	}
+	
+	return $null;
+}
 
 ###########################
 #
@@ -14,17 +27,17 @@ function Approve-Syntax {
 	param($path, [switch]$verbose)
 
 	if ($verbose) {
-			$VerbosePreference = ‘Continue’
+		$VerbosePreference = ‘Continue’
 	}
 
 	trap { Write-Warning $_; $false; continue }
 	& `
 	{
-			$contents = get-content $path
-			$contents = [string]::Join([Environment]::NewLine, $contents)
-			[void]$ExecutionContext.InvokeCommand.NewScriptBlock($contents)
-			Write-Verbose "Parsed without errors"
-			$true
+		$contents = get-content $path
+		$contents = [string]::Join([Environment]::NewLine, $contents)
+		[void]$ExecutionContext.InvokeCommand.NewScriptBlock($contents)
+		Write-Verbose "Parsed without errors"
+		$true
 	}
 }
 
@@ -35,16 +48,16 @@ function Approve-Syntax {
 ###########################
 
 function Update-Profile {
-    @(
-        $Profile.AllUsersAllHosts,
-        $Profile.AllUsersCurrentHost,
-        $Profile.CurrentUserAllHosts,
-        $Profile.CurrentUserCurrentHost
-    ) | % {
-        if(Test-Path $_){
-            Write-Verbose "Running $_"
-            . $_
-        }
-    }    
+	@(
+		$Profile.AllUsersAllHosts,
+		$Profile.AllUsersCurrentHost,
+		$Profile.CurrentUserAllHosts,
+		$Profile.CurrentUserCurrentHost
+	) % {
+		if(Test-Path $_){
+			Write-Verbose "Running $_"
+			. $_
+		}
+	}    
 }
 Set-Alias Reload-Profile Update-Profile
