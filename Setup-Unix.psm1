001a1db830a1a220cec30615c2f77119e6831531
Mike Glenn <mike.glenn@rammount.com>
2012-11-12 18:41:18 -0500

    add netstat

--- Setup-Unix.psm1
+++ Setup-Unix.psm1
@@ -121,4 +121,60 @@ function wc {
 	}
 }
 
-Export-ModuleMember grep, head, rm, wc -Alias which
+###########################
+#
+# netstat
+# http://blogs.microsoft.co.il/blogs/scriptfanatic/archive/2011/02/10/How-to-find-running-processes-and-their-port-number.aspx
+# Get-NetworkStatistics | where-object {$_.State -eq "LISTENING"} | Format-Table
+###########################
+
+function Get-NetworkStatistics 
+{ 
+    $properties = 'Protocol','LocalAddress','LocalPort' 
+    $properties += 'RemoteAddress','RemotePort','State','ProcessName','PID' 
+
+    netstat -ano | Select-String -Pattern '\s+(TCP|UDP)' | ForEach-Object { 
+
+        $item = $_.line.split(" ",[System.StringSplitOptions]::RemoveEmptyEntries) 
+
+        if($item[1] -notmatch '^\[::') 
+        {            
+            if (($la = $item[1] -as [ipaddress]).AddressFamily -eq 'InterNetworkV6') 
+            { 
+               $localAddress = $la.IPAddressToString 
+               $localPort = $item[1].split('\]:')[-1] 
+            } 
+            else 
+            { 
+                $localAddress = $item[1].split(':')[0] 
+                $localPort = $item[1].split(':')[-1] 
+            }  
+
+            if (($ra = $item[2] -as [ipaddress]).AddressFamily -eq 'InterNetworkV6') 
+            { 
+               $remoteAddress = $ra.IPAddressToString 
+               $remotePort = $item[2].split('\]:')[-1] 
+            } 
+            else 
+            { 
+               $remoteAddress = $item[2].split(':')[0] 
+               $remotePort = $item[2].split(':')[-1] 
+            }  
+
+            New-Object PSObject -Property @{ 
+                PID = $item[-1] 
+                ProcessName = (Get-Process -Id $item[-1] -ErrorAction SilentlyContinue).Name 
+                Protocol = $item[0] 
+                LocalAddress = $localAddress 
+                LocalPort = $localPort 
+                RemoteAddress =$remoteAddress 
+                RemotePort = $remotePort 
+                State = if($item[0] -eq 'tcp') {$item[3]} else {$null} 
+            } | Select-Object -Property $properties 
+        } 
+    } 
+}
+
+Set-Alias netstat Get-NetworkStatistics
+
+Export-ModuleMember grep, head, rm, wc -Alias which, netstat
