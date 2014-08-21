b13f9cab5abb509f63da91b86e0b07d34589740f
Mike Glenn <mglenn@ilude.com>
2014-08-21 18:43:16 -0400

    added chefdk devkit environment setup

--- profile.ps1
+++ profile.ps1
@@ -10,6 +10,12 @@ $script:pwd = Split-Path $MyInvocation.MyCommand.Path
 # 	}
 # }
 
+$devkit = "C:\opscode\chefdk\embedded\"
+if(Test-Path $devkit) {
+	$env:RI_DEVKIT = "$devkit"
+	$env:path = "$devkit\bin;$devkit\mingw\bin;$env:path"
+}
+
 ###########################
 #
 # Load all modules
