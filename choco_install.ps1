02092a53f23b1b1ce9281aeb7acb79ff64200272
Mike Glenn <mglenn@ilude.com>
2021-01-11 20:59:29 -0500

    Update choco_install.ps1

--- choco_install.ps1
+++ choco_install.ps1
@@ -1,15 +1,11 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y 7zip
-choco install -y ansicon
 choco install -y avidemux
 choco install -y open-shell -installArgs ADDLOCAL=StartMenu
 choco install -y ccleaner
 choco install -y ccenhancer
-choco install -y chefdk
-choco install -y chocolateygui --source https://www.myget.org/F/chocolateygui/ --pre
 choco install -y cobian-backup
-choco install -y ConEmu
-choco install -y curl
 choco install -y discord
 choco install -y displayfusion
 choco install -y docker-desktop
@@ -17,7 +13,6 @@ choco install -y DotNet3.5
 choco install -y DotNet4.6
 choco install -y dotnetfx
 choco install -y f.lux
-choco install -y git --params "/GitAndUnixToolsOnPath /NoShellIntegration"
 choco install -y googlechrome
 choco install -y handbrake
 choco install -y inkscape
@@ -27,12 +22,9 @@ choco install -y notepadplusplus
 choco install -y office365business
 choco install -y openvpn
 choco install -y paint.net
-choco install -y P4Merge
-# choco install -y postman
 choco install -y powershell-core
 choco install -y procexp
 choco install -y putty
-#choco install -y kitty
 choco install -y rsync
 choco install -y rufus
 choco install -y screenpresso
@@ -42,7 +34,6 @@ choco install -y sql-server-management-studio
 choco install -y synctrayzor
 choco install -y teamviewer
 choco install -y tor-browser
-choco install -y vagrant
 choco install -y vlc
 choco install -y unifying  
 choco install -y winscp
@@ -50,6 +41,18 @@ choco install -y wiztree
 
 choco install -y nvidia-geforce-now
 
+# Development Tools
+choco install -y ansicon
+choco install -y chefdk 
+choco install -y curl
+choco install -y git --params "/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration"
+choco install -y microsoft-windows-terminal
+choco install -y P4Merge
+choco install -y vagrant
+choco install -y wsl2
+choco install -y wsl-ubuntu-2004
+
+# VSCode and plugins
 choco install vscode
 choco install vscode-gitignore
 choco install vscode-ansible
