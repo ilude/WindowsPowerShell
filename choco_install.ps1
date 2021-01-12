371962a6f7f0fe440ed9c7debc724f83febf911c
Mike Glenn <mglenn@ilude.com>
2021-01-11 22:16:13 -0500

    Update choco_install.ps1

--- choco_install.ps1
+++ choco_install.ps1
@@ -41,25 +41,4 @@ choco install -y wiztree
 
 choco install -y nvidia-geforce-now
 
-# Development Tools
-choco install -y ansicon
-choco install -y chefdk 
-choco install -y curl
-choco install -y git --params "/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration"
-choco install -y microsoft-windows-terminal
-choco install -y P4Merge
-choco install -y vagrant
-choco install -y wsl2
-choco install -y wsl-ubuntu-2004
-
-# VSCode and plugins
-choco install vscode
-choco install vscode-gitignore
-choco install vscode-ansible
-choco install vscode-ruby
-choco install vscode-yaml
-choco install vscode-gitattributes
-choco install vscode-codespellchecker
-choco install vscode-gitlens
-choco install vscode-powershell
-choco install vscode-icons
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ilude/WindowsPowerShell/master/choco_install_development.ps1'))
