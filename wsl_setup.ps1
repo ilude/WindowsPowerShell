5fd6b6024b8a31e386110a23e656e69417041064
Mike Glenn <mglenn@ilude.com>
2021-12-08 11:55:43 -0500

    Create wsl_setup.ps1

---
+++ wsl_setup.ps1
@@ -0,0 +1,4 @@
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+wsl --set-default-version 2
+wsl --install -d Ubuntu-20.04
