3d8e5856569f1ff27ff7788900b6058a4836b528
Mike Glenn <mglenn@ilude.com>
2022-03-21 11:42:44 -0400

    Create choco_server_install.ps1

---
+++ choco_server_install.ps1
@@ -0,0 +1,13 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install -y 7zip
+choco install -y open-shell -installArgs ADDLOCAL=StartMenu
+choco install -y brave
+choco install -y ccleaner
+choco install -y notepadplusplus
+choco install -y powershell-core
+choco install -y procexp
+choco install -y t-clock
+choco install -y wiztree
+
+choco install -y teamviewer
