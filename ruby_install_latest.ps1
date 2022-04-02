6f412883c10e738cf0938945f9649b80e08b9228
Mike Glenn <mglenn@ilude.com>
2022-04-02 01:19:11 -0400

    Update ruby_install_latest.ps1

--- ruby_install_latest.ps1
+++ ruby_install_latest.ps1
@@ -1,6 +1,9 @@
 $url = ((Invoke-WebRequest "https://api.github.com/repos/oneclick/rubyinstaller2/releases" | ConvertFrom-Json)[0].assets | Where-Object name -match "^rubyinstaller-devkit-.*-x64.exe$").browser_download_url
 Invoke-WebRequest $url -Out rubyinstaller.exe
+
+$tag = (Invoke-WebRequest "https://api.github.com/repos/oneclick/rubyinstaller2/releases" | ConvertFrom-Json)[0].tag_name
 rubyinstaller.exe /silent /dir="c:/tools/$tag"
+
 refreshenv 
 ridk install 1 3
 ridk install 2
