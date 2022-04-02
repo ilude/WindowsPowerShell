3d6b5215b84c3467a7caff9ba1baca4af00a97bd
Mike Glenn <mglenn@ilude.com>
2022-04-01 20:52:12 -0400

    Create ruby_install_3.1.1.1.ps1

---
+++ ruby_install_3.1.1.1.ps1
@@ -0,0 +1,5 @@
+Invoke-WebRequest "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.1.1-1/rubyinstaller-3.1.1-1-x64.exe" -Out rubyinstaller.exe
+rubyinstaller.exe /silent /dir="c:/tools/ruby--3.1.1-1"
+Update-SessionEnvironment
+ridk install 1 3
+Update-SessionEnvironment
