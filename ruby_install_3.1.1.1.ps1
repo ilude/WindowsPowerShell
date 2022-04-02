e505e27c10016287be23b72cd2ce150205455711
Mike Glenn <mglenn@ilude.com>
2022-04-02 00:33:57 -0400

    Update ruby_install_3.1.1.1.ps1

--- ruby_install_3.1.1.1.ps1
+++ ruby_install_3.1.1.1.ps1
@@ -1,7 +1,7 @@
-Invoke-WebRequest "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.1.1-1/rubyinstaller-3.1.1-1-x64.exe" -Out rubyinstaller.exe
-rubyinstaller.exe /silent /dir="c:/tools/ruby--3.1.1-1"
-Update-SessionEnvironment
+Invoke-WebRequest "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.1.1-1/rubyinstaller-devkit-3.1.1-1-x64.exe" -Out rubyinstaller.exe
+./rubyinstaller.exe /silent /dir="c:/tools/ruby-3.1.1-1"
+$env:Path += ";c:\tools\ruby-3.1.1-1\bin"
 ridk install 1 3
-Update-SessionEnvironment
+ridk install 2
 
 rm rubyinstaller.exe
