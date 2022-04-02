147badf6bba3fd7956d532d4944c420de85bfa87
Mike Glenn <mglenn@ilude.com>
2022-04-02 01:17:35 -0400

    Update ruby_install_latest.ps1

--- ruby_install_latest.ps1
+++ ruby_install_latest.ps1
@@ -1,17 +1,8 @@
-Write-Host Determining latest release
-$tag = (Invoke-WebRequest "https://api.github.com/repos/oneclick/rubyinstaller2/releases" | ConvertFrom-Json)[0].tag_name
-# https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.1.1-1/rubyinstaller-3.1.1-1-x64.exe
-
-$filename = "$tag-x64.exe"
-$download = "https://github.com/oneclick/rubyinstaller2/releases/download/$tag/$tag-x64.exe"
-
-
-Write-Host Dowloading latest release
-Invoke-WebRequest $download -Out rubyinstaller.exe
-
+$url = ((Invoke-WebRequest "https://api.github.com/repos/oneclick/rubyinstaller2/releases" | ConvertFrom-Json)[0].assets | Where-Object name -match "^rubyinstaller-devkit-.*-x64.exe$").browser_download_url
+Invoke-WebRequest $url -Out rubyinstaller.exe
 rubyinstaller.exe /silent /dir="c:/tools/$tag"
-Update-SessionEnvironment
+refreshenv 
 ridk install 1 3
-Update-SessionEnvironment
+ridk install 2
 
 rm rubyinstaller.exe
