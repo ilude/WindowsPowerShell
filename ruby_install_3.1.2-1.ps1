85e37db1f5d605eef44c3429b6eccbccaeaec736
Mike Glenn <mglenn@ilude.com>
2022-04-21 16:44:58 -0400

    Update ruby_install_3.1.2-1.ps1

--- ruby_install_3.1.2-1.ps1
+++ ruby_install_3.1.2-1.ps1
@@ -1,3 +1,4 @@
+# Install the latest version of ruby with devkit 
 $ruby_version = '3.1.2-1'
 
 Invoke-WebRequest "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.1.2-1/rubyinstaller-devkit-$ruby_version-x64.exe" -Out rubyinstaller.exe
@@ -10,8 +11,10 @@ refreshenv
 rm ./rubyinstaller.exe
 refreshenv 
 
+# Install global gems
 gem install solargraph debug gitploy
 
+# Install mutagen for docker volumn shares that work with file system change notification
 Invoke-WebRequest  https://github.com/mutagen-io/mutagen/releases/download/v0.13.1/mutagen_windows_amd64_v0.13.1.zip -Out mutagen.zip
 Invoke-WebRequest  https://github.com/mutagen-io/mutagen-compose/releases/download/v0.13.1/mutagen-compose_windows_amd64_v0.13.1.zip -Out mutagen-compose.zip
 
@@ -22,3 +25,12 @@ rm mutagen-compose.zip
 
 cp mutagen*.* "C:\Tools\ruby-$ruby_version\bin\" -force
 rm mutagen*.*
+
+# install Chocolatey 
+iex((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+refreshenv
+choco install make -y
+
+# powershell quality of life improvements
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+PowerShellGet\Install-Module posh-docker -Scope CurrentUser -Force
