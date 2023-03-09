4d94fff70951eb9413d6b65e3f3bcbbca50faa62
Mike Glenn <mglenn@ilude.com>
2023-03-09 14:27:51 -0500

    Update choco_server_install.ps1

--- choco_server_install.ps1
+++ choco_server_install.ps1
@@ -3,15 +3,15 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 choco install -y 7zip
 choco install -y open-shell -installArgs ADDLOCAL=StartMenu
-#choco install -y brave
-#choco install -y ccleaner
 choco install -y notepadplusplus
 choco install -y powershell-core
+choco install -y rustdesk
+choco install -y wiztree
+
+#choco install -y brave
+#choco install -y ccleaner
 #choco install -y procexp
 #choco install -y t-clock
-choco install -y wiztree
-choco install -y rustdesk
-
 #choco install -y teamviewer
 
 # Activate Windows Server
