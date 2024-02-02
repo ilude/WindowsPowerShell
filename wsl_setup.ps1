86d986749d8eb37b2a7f219e3d57d90ec821cc63
Mike Glenn <219478+ilude@users.noreply.github.com>
2024-02-02 11:25:57 -0500

    Update wsl_setup.ps1

--- wsl_setup.ps1
+++ wsl_setup.ps1
@@ -1,4 +1,3 @@
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-wsl --set-default-version 2
-wsl --install -d Ubuntu-20.04
+wsl --install -d Ubuntu-22.04
